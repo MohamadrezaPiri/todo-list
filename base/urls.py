89f101711ef01220872f66a621c592775b7ba4cc
hamid piry <piry.mreza@gmail.com>
2022-08-13 16:53:56 +0430

    Initial commit

---
+++ base/urls.py
@@ -0,0 +1,15 @@
+from . import views
+from django.urls import path
+from django.contrib.auth.views import LogoutView
+
+urlpatterns=[
+    path('register',views.RegisterPage.as_view(),name='register'),
+    path('logout',LogoutView.as_view(next_page="login"),name='logout'),
+    path('login',views.CustomLoginView.as_view(),name='login'),
+    path('',views.TaskList.as_view(),name='tasks'),
+    path('task/<int:pk>',views.TaskDetail.as_view(),name='task'),
+    path('task-create',views.TaskCreate.as_view(),name='task-create'),
+    path('task-update/<int:pk>',views.TaskUpdate.as_view(),name='task-update'),
+    path('task-delete/<int:pk>',views.DeleteView.as_view(),name='task-delete'),
+    path('task-reorder/', views.TaskReorder.as_view(), name='task-reorder'),
+]
